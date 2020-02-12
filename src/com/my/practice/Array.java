d77939d1d881d3069dd20ec9c7ed1447da3248e4
Manzumul Haque <manzumul@metatude.com>
2020-02-12 15:55:55 +0600

    removed duplicate number

--- src/com/my/practice/Array.java
+++ src/com/my/practice/Array.java
@@ -1,27 +1,44 @@
 package com.my.practice;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Array {
-	private static final int LENGTH = 6;
+	private static final int LENGTH = 10;
 	private static final int MAX_TICKET_NUMBER = 69;
 	
 	public static void main(String[] args) {
 		int[] ticket = generateLotteryTicket();
+		Arrays.sort(ticket);
 		printTicket(ticket);
 	}
 	
 	public static int[] generateLotteryTicket() {
 		Random random = new Random();
 		int[] lotteryTicket = new int[LENGTH];
-		for(int i=0; i<LENGTH; i++) {
-			lotteryTicket[i] = random.nextInt(MAX_TICKET_NUMBER)+1;
+		int randomNumber;
+		for(int i = 0; i < LENGTH; i++) {
+			//lotteryTicket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+			do {
+				randomNumber = random.nextInt(MAX_TICKET_NUMBER) + 1;
+			}while(searchDuplicate(lotteryTicket, randomNumber));
+			
+			lotteryTicket[i] = randomNumber;
 		}
 		return lotteryTicket;
 	}
+	
 	public static void printTicket(int[] ticket) {
 		for(int i=0; i<LENGTH; i++) {
 			System.out.print(ticket[i] + "|");
 		}
+	}
+	
+	public static boolean searchDuplicate(int[] array, int numberToSearchFor) {
+		for(int value : array) {
+			if(value == numberToSearchFor)
+				return true;
+		}
+		return false;
 	}
 }
