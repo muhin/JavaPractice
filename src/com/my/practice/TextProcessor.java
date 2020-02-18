f790701ed44daf2696c6c99853edb008f8e3ec67
Manzumul Haque <manzumul@metatude.com>
2020-02-18 14:42:01 +0600

    text processor

--- src/com/my/practice/TextProcessor.java
+++ src/com/my/practice/TextProcessor.java
@@ -4,12 +4,33 @@ public class TextProcessor {
 
 	public static void main(String[] args) {
 		countWords("I love programming");
+		reverseText("Bangladesh");
+		System.out.println();
+		addSpaces("ILiveInDhaka");
 	}
 	
 	public static void countWords(String text) {
 		String[] words = text.split(" ");
 		int numberOfWords = words.length;
+		System.out.println("Your text contains " + numberOfWords + " words");
 		for(int i=0; i<numberOfWords; i++)
 			System.out.println(words[i]);
+	}
+	
+	public static void reverseText(String text) {
+		System.out.print("Reverse text is: ");
+		for(int i=text.length()-1; i>=0; i--)
+			System.out.print(text.charAt(i));
+	}
+	
+	public static void addSpaces(String text) {
+		StringBuilder modifiedText = new StringBuilder(text);
+		for(int i=0; i<modifiedText.length(); i++) {
+			if(i!=0 && Character.isUpperCase(modifiedText.charAt(i))) {
+				modifiedText.insert(i, " ");
+				i++;
+			}
+		}
+		System.out.println("Readable text is: " + modifiedText);
 	}
 }
