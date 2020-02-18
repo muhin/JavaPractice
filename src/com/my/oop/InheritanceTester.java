7adb8cbb3ea763ba7815b690619cdee553fb2757
Manzumul Haque <manzumul@metatude.com>
2020-02-18 16:18:34 +0600

    OOP

---
+++ src/com/my/oop/InheritanceTester.java
@@ -0,0 +1,10 @@
+package com.my.oop;
+
+public class InheritanceTester {
+
+	public static void main(String[] args) {
+		//Person person = new Person();
+		Employee employee = new Employee();
+	}
+
+}
